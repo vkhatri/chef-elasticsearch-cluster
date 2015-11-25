b6fba0fd46372fde0aece27ba159ec03d3e2f4af
Virender Khatri <vir.khatri@gmail.com>
2015-11-25 19:09:48 +0530

    #65, updated metadata for issues source url

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures ElasticSearch Cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.5.1'
 
+source_url 'https://github.com/vkhatri/chef-elasticsearch-cluster' if respond_to?(:source_url)
+issues_url 'https://github.com/vkhatri/chef-elasticsearch-cluster/issues' if respond_to?(:issues_url)
+
 %w(java yum apt).each do |d|
   depends d
 end
